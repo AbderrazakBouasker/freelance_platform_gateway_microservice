9e9764b63ba1a8978e5d3d0d0a92d3ee8014e35e
Jose Mokeni <jmmokeni@gmail.com>
2024-11-16 20:59:14 +0100

    config from server ok

--- src/main/java/com/microservice/gateway/GatewayApplication.java
+++ src/main/java/com/microservice/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RefreshScope
 public class GatewayApplication {
 
 	public static void main(String[] args) {
